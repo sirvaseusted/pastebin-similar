ad89d298cb17c5171fe11cf3355bc040d36430f6
Juan Antonio <sirvaseusted@gmail.com>
2019-05-14 20:29:53 +0100

    Actualizacion del WYSIWYG a Bootstrap y mejoras varias.

--- codigo/forms.py
+++ codigo/forms.py
@@ -9,6 +9,6 @@ class FormularioCodigo(ModelForm):
         fields = ['nombre', 'codigo', 'expiracion',]
         widgets = {
             'nombre': TextInput(attrs={'class':'form-control form-control-sm',  'required':'true'}),
-            'codigo': Textarea(attrs={'class':'form-control', 'required':'true'}),
+            'codigo': Textarea(attrs={'class':'textarea form-control', 'id':'content', 'required':'true', 'style':'width: 100%; height: 500px; font-size: 10px; line-height: 18px;'}),
             'expiracion': Select(attrs={'type':'select', 'class':'form-control form-control-sm', 'required':'true'}),
-            } 
+            }
